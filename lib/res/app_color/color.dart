74c5fbc5466e504556ee3d5b15b7d9a59bbcf91a
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-25 08:08:04 +0600

    add app color

---
+++ lib/res/app_color/color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color primaryColor = Color(0xFFF0F0EF);
+  static Color secondaryColor = Color(0xFFEAE6DF);
+  static Color bgColor = Color(0xFFFBFBFD);
+  static Color textColor = Color(0xFF583354);
+  static Color iconColor = Color(0xFFFF6766);
+  static Color splineColor = Color.fromARGB(255, 210, 163, 96);
+  static Color secondaryBgColor = Color.fromARGB(255, 238, 240, 242);
+
+  static const defaultPadding = 16.0;
+}
