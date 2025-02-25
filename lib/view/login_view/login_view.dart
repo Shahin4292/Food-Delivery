1f035db14fb508e3bad5b79cdca47974d04c9d4f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-25 08:46:49 +0600

    added email and password input field

--- lib/view/login_view/login_view.dart
+++ lib/view/login_view/login_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/res/assets_path/assets_path.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../res/app_color/color.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -10,6 +12,8 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  bool isCheck = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +24,66 @@ class _LoginViewState extends State<LoginView> {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: defaultPadding,
             children: [
               Text(
                 "Welcome Back",
                 style: GoogleFonts.ubuntu(
-                    color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600),
               ),
+              SizedBox(height: defaultPadding),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: defaultPadding * 2),
+                padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 1, color: Colors.black54),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      // prefixIcon: Container(
+                      //   height: 10,
+                      //   width: 10,
+                      //   child: Align(
+                      //     alignment: Alignment.centerLeft,
+                      //     child: Icon(Icons.email_outlined,
+                      //         color: Colors.black, size: 25),
+                      //   ),
+                      // ),
+                      disabledBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Email address",
+                      hintStyle: TextStyle(color: Colors.black26)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: defaultPadding * 2),
+                padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 1, color: Colors.black54),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      // prefixIcon: Container(
+                      //   height: 10,
+                      //   width: 10,
+                      //   child: Align(
+                      //     alignment: Alignment.centerLeft,
+                      //     child: Icon(Icons.email_outlined,
+                      //         color: Colors.black, size: 25),
+                      //   ),
+                      // ),
+                      disabledBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Enter password",
+                      hintStyle: TextStyle(color: Colors.black26)),
+                ),
+              )
             ],
           ),
           Spacer(),
